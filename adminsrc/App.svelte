44b5d6f8bb4bc3b8aac5d229b75383399d5b2cf2
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2020-07-04 16:51:57 +0530

    admin

--- adminsrc/App.svelte
+++ adminsrc/App.svelte
@@ -1,32 +1,23 @@
 <script>
-	import { Router, Link, Route } from "svelte-routing";
-	  import Dashboard from "./pages/dashboard.svelte";
-	  import Pages from "./pages/pages.svelte";
-	  import Blog from "./pages/blog.svelte";
-	 
-	
-	  export let url = "";
-</script>
-<style>
-	.body{font-size:14px;}
-</style>
+  import { Router, Link, Route } from "svelte-routing";
+  import Dashboard from "./pages/dashboard.svelte";
+  import Pages from "./pages/pages.svelte";
+  import Blog from "./pages/blog.svelte";
+ 
+  export let url = "";
+</script> 
+ 
 <Router url="{url}">
-	<header class="py-3 bg-white">
-		<div class="container">
-			<div class="header">
-				<nav class="navbar navbar-expand-md navbar-light px-0">
-					<Link to="/">Homeww</Link>
-					<Link to="about">About</Link>
-					<Link to="blog">Blog</Link>
-				</nav>
-			</div>
-		</div>
-	</header>test
-	<div>
-		<Route path="blog" component="{Blog}" />
-		<Route path="pages" component="{Pages}" />
-		<Route path="/">
-			<Dashboard />
-		</Route>
-	</div>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+    <Link to="blog">Blog</Link>
+  </nav>
+  test
+  <div>
+  
+    <Route path="blog" component="{Blog}" />
+    <Route path="pages" component="{Pages}" />
+    <Route path="/"><Dashboard /></Route>
+  </div>
 </Router>
